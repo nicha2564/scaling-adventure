b4487aa00f15558cdcf85e232f56f280b2082ab3
nicha2564 <nicha2564@outlook.com>
2021-09-02 18:14:35 +0700

    prepare gh-pages

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-import './style.css'
+import './style.css';
+import './main.css';
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+document.querySelector('#main').innerHTML = `
+  <h1>Hello scaling-adventure!</h1>
+
+`;
